54bbe31da9f2fd73ae1a72f35e5d9173d7672740
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2021-05-20 18:32:00 -0300

    Add confidence interval, ztests, and ttests

--- AyVD/lab/part2/docs.py
+++ AyVD/lab/part2/docs.py
@@ -1,6 +1,7 @@
 # %%
 import numpy as np
 import pandas as pd
+import statsmodels.stats.api as sms
 
 URL = "https://cs.famaf.unc.edu.ar/~mteruel/datasets/diplodatos/sysarmy_survey_2020_processed.csv"
 DB = pd.read_csv(URL)
@@ -49,16 +50,36 @@ groupA.mean() - groupB.mean()
 # \frac{\sigma_B^2}{n_B}}$
 #
 # %%
-sigma = np.sqrt(groupA.std()**2 / groupA.size + groupB.std()**2 / groupB.size)
+np.sqrt(groupA.std()**2 / groupA.size + groupB.std()**2 / groupB.size)
 # %% [markdown]
 # Notar que la diferencia de las medias del salario de ambos de grupos es de
 # \$23262 con un error de estimación de alrededor de los \$2400, es decir, que
 # la estimación realizada tiene un bajo desvío, lo cual nos indicaría que tiene
-# buena precisión lo cual podría deberse al gran tamaño de las muestras
-# %%
+# buena precisión lo cual podría deberse al gran tamaño de las muestras.
 
-left = groupA.mean() - groupB.mean() - 1.96 * sigma
-right = groupA.mean() - groupB.mean() + 1.96 * sigma
-(left, right)
+# %% [markdown]
+# ## Intervalo de Confianza para $\mu_A - \mu_B$
+# Ahora compararemos la estimación puntual obtenida con la de un intervalo de
+# confianza cuyo nivel de significancia es de $1 - \alpha = 0.95$. Notar que el
+# tamaño de la muestra para el grupo A es de 4815 y el del grupo B de 891, por
+# lo tanto resultaría conveniente calcular el intervalo de confianza utilizando
+# un estadistico Z.
+# %%
+cm = sms.CompareMeans(sms.DescrStatsW(groupA), sms.DescrStatsW(groupB))
+cm.zconfint_diff(alpha=alpha, usevar='unequal')
+# %% [markdown]
+# Con un 95% de confianza se espera que la diferencia de salarios medios entre
+# el grupo A y B este comprendido entre \$18561 y \$27964. Es decir, si
+# generamos sucesivos intervalos por medio de este estadistico, el 95% de ellos
+# van a contener el parámetro. Entonces hay una chance del 95% de que el
+# obtenido sea uno de esos intervalos.
+# 
+
+# %%
+cm.tconfint_diff(alpha=alpha, usevar='unequal')
+
+
+# %%
+cm.ztest_ind(alternative="larger", usevar="unequal")
 
 # %%
