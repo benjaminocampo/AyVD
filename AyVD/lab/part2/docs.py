2b0b14c297b3272c8f0544a17ebe3e2c50af23b3
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2021-05-21 13:04:48 -0300

    Explain tests hypothesis

--- AyVD/lab/part2/docs.py
+++ AyVD/lab/part2/docs.py
@@ -21,6 +21,11 @@ is_man = df.profile_gender == 'Hombre'
 groupA = df[(df[salary_monthly_NETO] > 1000) & is_man][salary_monthly_NETO]
 groupB = df[(df[salary_monthly_NETO] > 1000) & ~is_man][salary_monthly_NETO]
 
+# %%
+groupA
+
+# %%
+groupB
 
 # %% [markdown]
 # ## Estimación Puntual
@@ -33,12 +38,13 @@ groupA.mean() - groupB.mean()
 
 # %% [markdown]
 # Notar que la diferencia de las medias del salario de ambos de grupos es de
-# \$23262 o equivalentemente:
+# \$23262. A su vez podemos ver de cuanto es esta diferencia relativo al salario
+# medio del grupo A:
 
 # %% 
 (groupA.mean() - groupB.mean()) / groupA.mean() * 100
 # %% [markdown]
-# El grupo de hombres cobran casi un 23% más del salario neto que el conformado por las mujeres y otros.
+# Esto nos dice que el grupo de hombres cobran casi un 23% más del salario neto que el conformado por las mujeres y otros.
 
 # %% [markdown]
 # Aparte de reportar la estimación puntual se calculo el error estandar como una
@@ -55,15 +61,18 @@ np.sqrt(groupA.std()**2 / groupA.size + groupB.std()**2 / groupB.size)
 # Notar que la diferencia de las medias del salario de ambos de grupos es de
 # \$23262 con un error de estimación de alrededor de los \$2400, es decir, que
 # la estimación realizada tiene un bajo desvío, lo cual nos indicaría que tiene
-# buena precisión lo cual podría deberse al gran tamaño de las muestras.
+# buena precisión probablemente debido al gran tamaño de las muestras.
 
 # %% [markdown]
 # ## Intervalo de Confianza para $\mu_A - \mu_B$
 # Ahora compararemos la estimación puntual obtenida con la de un intervalo de
-# confianza cuyo nivel de significancia es de $1 - \alpha = 0.95$. Notar que el
-# tamaño de la muestra para el grupo A es de 4815 y el del grupo B de 891, por
-# lo tanto resultaría conveniente calcular el intervalo de confianza utilizando
-# un estadistico Z.
+# confianza cuyo nivel de significancia es de $1 - \alpha = 0.95$. El
+# estadistico utilizado es $\frac{\overline X_A - \overline X_B - \mu_A -
+# \mu_B}{\sqrt{{\frac{S_A^2}{n_A} + \frac{S_B^2}{n_B}}}}$
+#
+# Ahora bien, como el tamaño de la muestra para el grupo A es de 4815 y el del
+# grupo B de 891, por el TCL, el estadistico tiene distribución aprox. normal.
+# Obteniendo el siguiente intervalo de confianza:
 # %%
 cm = sms.CompareMeans(sms.DescrStatsW(groupA), sms.DescrStatsW(groupB))
 cm.zconfint_diff(alpha=alpha, usevar='unequal')
@@ -73,13 +82,69 @@ cm.zconfint_diff(alpha=alpha, usevar='unequal')
 # generamos sucesivos intervalos por medio de este estadistico, el 95% de ellos
 # van a contener el parámetro. Entonces hay una chance del 95% de que el
 # obtenido sea uno de esos intervalos.
-# 
 
-# %%
+# %% [markdown]
+# Similarmente más allá del tamaño de la muestra, nuestro estadistico tiene
+# distribución t-student y por ende también podemos realizar un análisis sin
+# suponer normalidad. Aún aborando de esta manera obtenemos un resultado
+# similar.
 cm.tconfint_diff(alpha=alpha, usevar='unequal')
-
+# %% [markdown]
+# ## Tests de Hipótesis
+# Para este caso querremos ver si podemos afirmar con una cierta significancia
+# que el salario medio neto del grupo A es superior al del grupo B. Para ellos
+# planteamos el siguiente test de hipótesis.
+# 
+# $H_0: \mu_A - \mu_B = 0$
+#
+# $H_a: \mu_A - \mu_B > 0$
+#
+# Nuestro estadistico a utilizar en este test es el mismo que para los
+# intervalos de confianza. Suponemos inicialmente $H_0$, es decir que el salario
+# medio de ambos grupos son iguales. Bajo $H_0$ el estadístico tiene:
+#
+#   - Distribución aprox. normal, considerando el tamaño de la muestra y el TCL.
+#   - Distribución t-student.
+#
+#
+# Por lo tanto procederemos a realizar tanto z como t tests para ver que
+# para ambos tenemos resultados similares.
+# Consideramos un nivel de significancia similar del 95% y suponiendo varianzas
+# distintas de ambos grupos.
 
 # %%
-cm.ztest_ind(alternative="larger", usevar="unequal")
+ztstat, zpvalue = cm.ztest_ind(alternative="larger", usevar="unequal")
+(ztstat, zpvalue)
+# %% [markdown]
+# `ztstat` es el valor observado del estadistico y `zpvalue` es el p-valor
+# obtenido. Luego para concluir con el test solo queda ver que:
+#   
+#   - Rechazar $H_0$ si $pvalue \leq \alpha$
+#   - No rechazar $H_0$ si $pvalue \gt \alpha$
+# %%
+zpvalue <= alpha
+
+# %% [markdown]
+# Claramente el p-valor es menor por una gran diferencia y por ende afirmamos que
+# con un 95% de confianza el salario medio del grupo A es mayor que el del B.
+
+# %% [markdown]
+# Si ahora realizamos el mismo test de hipotesis bajo la suposición de $H_0$ y
+# distribución t-student por parte de nuestro estadistico, obtenemos:
+# %%
+ttstat, tpvalue, df = cm.ttest_ind(alternative="larger", usevar="unequal")
+
+(ttstat, tpvalue, df)
+# %% [markdown]
+# Notar que el valor observado `ttstat` es el mismo que el anterior `ztstat`
+# pues las expresiones de los estadisticos son las mismas. Lo que cambia es cual
+# es la distribución a considerar durante el test y por ende los valores
+# criticos para el calculo del pvalor. `df` Nos indica los grados de libertad
+# utilizada en el test, recordar que a mayor grados de libertad, nuestro
+# estadístico tendrá más similitud a una normal. Finalmente para concluir el
+# test, también rechazamos $H_0$.
+
+# %%
+tpvalue <= alpha
 
 # %%
